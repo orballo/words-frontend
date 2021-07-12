e6635eacf45489e68594e409fa0c6b9097aa649a
Eduardo Campaña <asrielo@gmail.com>
2021-07-12 16:35:44 +0900

    wip

--- packages/words-theme/src/components/main.tsx
+++ packages/words-theme/src/components/main.tsx
@@ -27,7 +27,7 @@ const Main: React.FC = () => {
   }, [state.auth.isSynced, state.auth.user]);
 
   const mainStyles = css`
-    max-width: 640px;
+    ${state.router.isHome ? "" : "max-width: 640px;"}
     min-height: 100%;
     margin: auto;
   `;
